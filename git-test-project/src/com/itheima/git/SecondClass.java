ae2f0d6943703eacb776e1f0120c6899dcb0cd44
Ghh <1191336892@qq.com>
2019-08-23 21:38:09 +0800

    第二次修改

---
+++ git-test-project/src/com/itheima/git/SecondClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+        int i = 0;
+        i += 10;
+        System.out.println("值为： " + i);
+    }
+}
